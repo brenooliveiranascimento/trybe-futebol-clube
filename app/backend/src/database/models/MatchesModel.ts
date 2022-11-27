196dfb13f7f95223229df71e01d4476b23d709b2
Breno Nascimento <satsukebare23@gmail.com>
2022-11-27 17:00:54 -0300

    fix

--- app/backend/src/database/models/MatchesModel.ts
+++ app/backend/src/database/models/MatchesModel.ts
@@ -1,6 +1,6 @@
 import { Model, INTEGER, BOOLEAN } from 'sequelize';
 import db from '.';
-import Team from './TeamModel';
+import Teams from './TeamsModel';
 
 class Matches extends Model {
   id!: number;
@@ -45,10 +45,10 @@ Matches.init({
   timestamps: false,
 });
 
-Matches.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
-Matches.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
 
-Team.hasMany(Matches, { foreignKey: 'homeTeam', as: 'teamHome' });
-Team.hasMany(Matches, { foreignKey: 'awayTeam', as: 'teamAway' });
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'teamHome' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'teamAway' });
 
 export default Matches;
--- app/backend/src/database/models/TeamsModel.ts
+++ app/backend/src/database/models/TeamsModel.ts
@@ -1,12 +1,12 @@
 import { Model, INTEGER, STRING } from 'sequelize';
 import db from '.';
 
-class Team extends Model {
+class Teams extends Model {
   id!: number;
   teamName!: string;
 }
 
-Team.init({
+Teams.init({
   id: {
     type: INTEGER,
     allowNull: false,
@@ -20,8 +20,8 @@ Team.init({
 }, {
   underscored: true,
   sequelize: db,
-  modelName: 'Team',
+  modelName: 'Teams',
   timestamps: false,
 });
 
-export default Team;
+export default Teams;
