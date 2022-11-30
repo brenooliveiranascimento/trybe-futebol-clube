5963e45779a697d9fa8b4fbd39f51095150e5486
Breno Nascimento <satsukebare23@gmail.com>
2022-11-30 13:15:57 -0300

    req 26

--- app/backend/src/services/MatchesServices.ts
+++ app/backend/src/services/MatchesServices.ts
@@ -1,9 +1,11 @@
+import { ITeams } from '../interface/ITeams';
 import { IAddMatches, IMatches } from '../interface/IMatches';
 import Matches from '../database/models/MatchesModel';
 import Teams from '../database/models/TeamsModel';
+import CustomError from '../utils/StatusError';
 
 export default class MatchesService {
-  constructor(private matchesModel = Matches) {}
+  constructor(private matchesModel = Matches, private teamModel = Teams) {}
 
   async getAll(): Promise<IMatches[]> {
     const matches = await this.matchesModel.findAll({
@@ -28,7 +30,19 @@ export default class MatchesService {
     return matches;
   }
 
+  async verifyTeams(teams: number[]): Promise<boolean> {
+    const checkTeams = await Promise.all(teams.map((currTeam: number) => this
+      .teamModel.findByPk(currTeam))) as ITeams[];
+    return checkTeams.every((currTeam: ITeams | boolean) => currTeam);
+  }
+
   async addMatch(matchData: IAddMatches): Promise<IAddMatches> {
+    const { awayTeam, homeTeam } = matchData;
+    const validateTeams = await this.verifyTeams([awayTeam, homeTeam]);
+    console.log(validateTeams);
+    if (!validateTeams) {
+      throw new CustomError('There is no team with such id!', 404);
+    }
     const add = await this.matchesModel.create({
       ...matchData, inProgress: true,
     });
